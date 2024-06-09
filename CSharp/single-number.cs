1588b3744a20e349f38ba0f7c06436cd436bcd0e
Marcel Veldhuizen <marcel@subbot.net>
2024-06-09 10:25:11 +0200

    Add two solution for Easy problems
    https://leetcode.com/problems/single-number/
    https://leetcode.com/problems/reverse-bits/

---
+++ CSharp/single-number.cs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/single-number/
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result = 0;
+        foreach (int num in nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
